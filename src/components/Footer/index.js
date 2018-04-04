ae45dff09d2c3d5d1937fa0dee1a5bec72d5e3c6
chuiliu <zhengchuiliu@foxmail.com>
2018-04-04 17:56:25 +0800

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import './index.css'
 
 const Footer = () => (
     <footer className="footer">power by <a href="https://github.com/gatsbyjs/gatsby" target="_blank">gatsby</a></footer>
